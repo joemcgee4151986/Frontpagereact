6ad82ca81732472a2390ca342355b93d4acc16d9
unknown <jrmcgee@asu.edu>
2022-06-21 15:55:36 +0900

    Trimmed the ui for the mainsection & two

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -60,15 +60,15 @@ export default function About() {
               <h2>Joseph McGee</h2>
               <h3>Percipit maiestatis sea eu</h3>
               <p>While I'm not programming, I like to relax, exercise, and play video games when my son is sleeping.</p>
-              <p>Eum illum nulla graeci at, mea quis munere indoctum at. In sea partiendo hendrerit. Quaestio partiendo an eam, rebum vitae accumsan ius id. Duo at causae option.</p>
+              {/*<p>Eum illum nulla graeci at, mea quis munere indoctum at. In sea partiendo hendrerit. Quaestio partiendo an eam, rebum vitae accumsan ius id. Duo at causae option.</p>
               <p>At persius imperdiet vis, ea elit atqui aperiri mei, percipit maiestatis sea eu. Has et partem hendrerit, vim cibo veniam aliquid an. No pri populo abhorreant, everti mandamus ne mea. Debitis forensibus suscipiantur ius cu. Ei per possim verterem, et iudico voluptatum eos.</p>
               <h3>Nam option recusabo</h3>
               <p>Te mel meis adhuc. Choro percipit mei eu, fabulas fuisset tibique ad sea, cu eos sint falli iracundia. Usu ex minimum corrumpit, postea dolores salutandi ne est, cu nam option recusabo reprehendunt. Prima vocibus argumentum ex usu. Nam te legere salutatus dissentiunt, his ei principes prodesset, est possit blandit ex.</p>
               <p>Pro no rebum timeam necessitatibus, et mnesarchum quaerendum has. Duo molestie interesset at. Vel ad legere populo. Sed ne saepe doming perpetua. Omnis iuvaret volumus an duo, qui duis audiam fabellas in.</p>
               <p>Te has amet modo perfecto, te eum mucius conclusionemque, mel te erat deterruisset. Duo ceteros phaedrum id, ornatus postulant in sea. His at autem inani volutpat. Tollit possit in pri, platonem persecuti ad vix, vel nisl albucius gloriatur no.</p>
-                
+      */}
               <h2>Michael McGee</h2>
-              <p>Ea duo atqui incorrupte, sed rebum regione suscipit ex, mea ex dicant percipit referrentur. Dicat luptatum constituam vix ut. His vide platonem omittantur id, vel quis vocent an. Ad pro inani zril omnesque. Mollis forensibus sea an, vim habeo adipisci contentiones ad, tale autem graecis ne sit.</p>
+      <p>Ea duo atqui incorrupte, sed rebum regione suscipit ex, mea ex dicant percipit referrentur. Dicat luptatum constituam vix ut. His vide platonem omittantur id, vel quis vocent an. Ad pro inani zril omnesque. Mollis forensibus sea an, vim habeo adipisci contentiones ad, tale autem graecis ne sit.</p>
               {/*Article section ends*/}
               <div className="lavenderBox">
                 <div className="header">Panel title</div>
@@ -83,9 +83,9 @@ export default function About() {
               </div>
             </div>
             <div className="pagefooter">
-              This page was last edited on 29.07.2017 | Template by <a href="/http://html5-templates.com/" target="_blank" rel="noopener noreferrer">HTML5 Templates</a> {/* Please leave this link unchanged */}
+             Template by <a href="/http://html5-templates.com/" target="_blank" rel="noopener noreferrer">HTML5 Templates</a> {/* Please leave this link unchanged */}
               <div className="footerlinks">
-                <a href="/">Privacy policy</a> <a href="/">About</a> <a href="/">Terms and conditions</a> <a href="/">Cookie statement</a> <a href="/">Developers</a>
+                
                
               </div>
               </div>
--- src/pages/mainsection/Mainsection.js
+++ src/pages/mainsection/Mainsection.js
@@ -56,12 +56,12 @@ export default function Mainsection() {
             {/*main section ends*/}
             {/*Article section starts */}
             <div className="article">
-              <h1>nope</h1>
+              <h1>Virus</h1>
               <p className="siteSub">From Wikipedia, the free encyclopedia</p>
               <p className="roleNote">This article is about the writing implement. For other uses, see Pencil (disambiguation).</p>
               <div className="articleRight">
                 <div className="articleRightInner">
-                  <img src="https://thecatapi.com/api/images/get?format=src&type=gif" alt="meteors" />
+                  {/*<img src="https://thecatapi.com/api/images/get?format=src&type=gif" alt="meteors" />*/}
                 </div>
                 <p>{post.title}</p>
               </div>
