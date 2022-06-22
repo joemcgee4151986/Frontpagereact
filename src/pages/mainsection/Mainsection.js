de083b7b088f830acbf91c93e24a40ce942b6fd7
unknown <jrmcgee@asu.edu>
2022-06-22 12:58:14 +0900

    added a homepage folder

--- src/pages/mainsection/Mainsection.js
+++ src/pages/mainsection/Mainsection.js
@@ -56,7 +56,7 @@ export default function Mainsection() {
             {/*main section ends*/}
             {/*Article section starts */}
             <div className="article">
-              <h1>Virus</h1>
+              <h1  contentEditable= "true" className="title">Virus</h1>
               <p className="siteSub">From Wikipedia, the free encyclopedia</p>
               <p className="roleNote">This article is about the writing implement. For other uses, see Pencil (disambiguation).</p>
               <div className="articleRight">
