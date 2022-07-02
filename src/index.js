75b93fe31ffe09f94b79ce228b642717c93c360d
joe mcgee <jrmcgee@asu.edu>
2022-07-02 22:21:50 +0900

    ran npm run build to build an optimized build

--- src/index.js
+++ src/index.js
@@ -10,18 +10,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
     <Sidebar/>
-  
-  <App/>
- 
-  
-    
-    
-    
+    <App/> 
     </React.StrictMode>
-
- 
 );
 
   
@@ -31,21 +22,5 @@ root.render(
    
    
       
-     
-    
-   
- 
-    
-      
-    
 
-  
-
-  
- 
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
