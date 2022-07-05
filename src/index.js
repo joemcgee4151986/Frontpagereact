276486f81e2b6ec6c2f4902894e8c6e438233665
unknown <jrmcgee@asu.edu>
2022-07-05 16:17:06 +0900

    added components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'
 import Sidebar from "./pages/sidebar/Sidebar";
 import reportWebVitals from './reportWebVitals';
 
--- src/pages/covid/covid.js
+++ src/pages/covid/covid.js
@@ -1,113 +1,16 @@
 import React from "react";
-import styles from "./covid.module.css";
-import axios from "axios";
-import { HashLink } from "react-router-hash-link";
-import { Outlet } from "react-router-dom";
-const baseURL = "https://desolate-sea-33600.herokuapp.com/users/first";
-export default function Mainsection() {
-  const [post, setPost] = React.useState(null);
+import "./Covid.module.css"
 
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
+import Body from "../body/body";
 
-  if (!post) return null;
+export default function Covid() {
 
   return (
     <div>
-      <div className={styles.mainsection}>
-        <div className={styles.headerLinks}></div>
-        <div className={`${styles.tabs} ${styles.clearfix}`}>
-          <div className={styles.tabsLeft}>
-            <ul></ul>
-          </div>
+      <Body/>
 
-          <div className={styles.tabsRight}>
-            <ul></ul>
-          </div>
-        </div>
-
-        <div className={styles.article}>
-          <h1 className={styles.title}>{post.au_fname}</h1>
-
-          <p className={styles.siteSub}>
-            From Wikipedia, the free encyclopedia
-          </p>
-          <p className={styles.roleNote}>
-            This article is about the writing implement. For other uses, see
-            Pencil (disambiguation).
-          </p>
-          <div className={styles.articleRight}>
-            <div className={styles.articleRightInner}>
-              <img
-                src="https://res.cloudinary.com/dtpgi0zck/image/upload/s--7UXxoGoz--/c_fill,h_580,w_860/v1/EducationHub/photos/virus.webp"
-                alt="meteors"
-              />
-            </div>
-            <p>{post.au_fname}</p>
-          </div>
-          <div>
-            <p id="information">{post.au_id}</p>
-          </div>
-
-          <div className={styles.contentsPanel}>
-            <div className={styles.contentsHeader}>Contents</div>
-            <ul>
-              <li>
-                <span>1</span>
-                <a href="/">History</a>
-                <ul>
-                  <li>
-                    <span>1.1</span>
-                    <HashLink smooth to="#information">
-                      First
-                    </HashLink>{" "}
-                  </li>
-                  <li>
-                    <span>1.2</span>
-                    <a href="/">Discovery of graphite deposit</a>{" "}
-                  </li>
-                  <li>
-                    <span>1.3</span>
-                    <a href="/">Wood holders added </a>
-                  </li>
-                  <li>
-                    <span>1.4</span>
-                    <a href="/">The pencil in America </a>
-                  </li>
-                  <li>
-                    <span>1.5</span>
-                    <a href="/">Eraser attached </a>
-                  </li>
-                  <li>
-                    <span>1.6</span>
-                    <a href="/">Marking material </a>
-                  </li>
-                  <li>
-                    <span>1.7</span>
-                    <a href="/">Pencil extenders </a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <span>2</span>
-                <a href="/">Health</a>
-              </li>
-              <li>
-                <span>3</span>
-                <a href="/">Manufacture</a>
-              </li>
-              <li>
-                <span>4</span>
-                <a href="/">Grading and classification</a>
-              </li>
-            </ul>
-          </div>
-          <Outlet />
-        </div>
-      </div>
+   
     </div>
   );
-}
+
+  }
