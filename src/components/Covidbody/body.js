71c68d87fb639a5e6cab51011ec47ea776b8298c
joe mcgee <jrmcgee@asu.edu>
2022-07-09 14:20:22 +0900

    added mobile button

--- src/components/Covidbody/body.js
+++ src/components/Covidbody/body.js
@@ -59,61 +59,29 @@ return (
                     <li>
                       <span>1</span>
                       <a href="/">History</a>
-                      <ul>
+                     
                         <li>
                           <span>1.1</span>
                           <HashLink smooth to="#information">
                             First
                           </HashLink>
                         </li>
-                        <li>
-                          <span>1.2</span>
-                          <a href="/">Discovery of graphite deposit</a>{" "}
                         </li>
-                        <li>
-                          <span>1.3</span>
-                          <a href="/">Wood holders added </a>
-                        </li>
-                        <li>
-                          <span>1.4</span>
-                          <a href="/">The pencil in America </a>
-                        </li>
-                        <li>
-                          <span>1.5</span>
-                          <a href="/">Eraser attached </a>
-                        </li>
-                        <li>
-                          <span>1.6</span>
-                          <a href="/">Marking material </a>
-                        </li>
-                        <li>
-                          <span>1.7</span>
-                          <a href="/">Pencil extenders </a>
-                        </li>
+                      
                       </ul>
-                    </li>
-                    <li>
-                      <span>2</span>
-                      <a href="/">Health</a>
-                    </li>
-                    <li>
-                      <span>3</span>
-                      <a href="/">Manufacture</a>
-                    </li>
-                    <li>
-                      <span>4</span>
-                      <a href="/">Grading and classification</a>
-                    </li>
-                  </ul>
-                </div>
+              
+                  </div>
+                     </div>
+                     </div>
+                     </div>
+);
+}
       
                 
-              </div>
-            </div>
-          </div>
-        );
-      }
-      
+              
+            
+          
+
         
         
           
--- src/pages/sidebar/Sidebar.js
+++ src/pages/sidebar/Sidebar.js
@@ -4,24 +4,32 @@ export default function Sidebarvisual() {
     return (
       <div>
     <div className="wrapAll clearfix">
-          <div className={styles.sidebar}>
-            <div className="logo">
-              <a href="/"><img src alt="" /></a>
-            </div>
-         <ul>
+    <div className={styles['mobile-menu']}>
+    <label for={styles['show-menu']} classname={styles['show-menu']}><div class={styles.lines}></div>
+    <input type="checkbox" id={styles['show-menu']} />
+    <ul id={styles.menu}>
                 <li><a href = ".">Home</a></li>
                 <li><a href="./About">About</a></li>
                 <li><a href="./Covid">Covid</a></li>
                 <li><a href="./Mainsectiontwo">Virus</a></li>
               </ul>
+ 
+       {/*   <div className={styles.sidebar} />*/}
+            <div className="logo">
+              <a href="/"><img src alt="" /></a>
+            </div>
+         
+             
+              </label>
             </div>
           </div>
           </div>
-         
-
+       
+ 
      
-    
+   
      
-      
+     
           );
           }
+
